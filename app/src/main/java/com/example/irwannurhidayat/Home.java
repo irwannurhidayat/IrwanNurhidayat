c198278fa6cdebe683f3ddfb619306f696295f6a
irwannurhidayat <irwan.nurhidayat11@gmail.com>
2023-03-10 23:13:49 +0700

    penambahan dashboard

---
+++ app/src/main/java/com/example/irwannurhidayat/Home.java
@@ -0,0 +1,19 @@
+package com.example.irwannurhidayat;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+    }
+}
--- app/src/main/java/com/example/irwannurhidayat/MainActivity.java
+++ app/src/main/java/com/example/irwannurhidayat/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.irwannurhidayat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.txtUser);
+        TextView password = (TextView) findViewById(R.id.txtpassword);
+
+        Button login = (Button) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("irwan") && password.getText().toString().equals("irwan")) {
+                //correct
+                    startActivity(new Intent(MainActivity.this,Home.class));
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                }else
+                //incorrect
+                    Toast.makeText(MainActivity.this,"Login Failed!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
